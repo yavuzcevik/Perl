a8b35fe4dc7dba76acb9d6008aa4d46aa2da638a
Halil Yavuz Çevik <yvzcevik@gmail.com>
2018-05-29 22:51:42 +0300

    Update printToVariable.pl

--- Code_Fragments/printToVariable.pl
+++ Code_Fragments/printToVariable.pl
@@ -1,7 +1,6 @@
 #!/usr/bin/perl
-# Assign a value to a variable with print function
+# The code can assign given value of "file_handler" parameter to the "output" parameter
 my $output;
 open my $file_handler, '>', \$output or die "Can't open the file $!";
 print $file_handler "Success!\n";
 print $output;
-

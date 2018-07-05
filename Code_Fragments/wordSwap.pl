a13b279731f6642c63771ab6d5ae287bcd55e272
Halil Yavuz Çevik <yvzcevik@gmail.com>
2018-07-05 16:28:24 +0300

    Creating "output.txt" and writing in it

--- Code_Fragments/wordSwap.pl
+++ Code_Fragments/wordSwap.pl
@@ -18,7 +18,12 @@ EOF
 
 my $full_path = <STDIN>;
 chomp($full_path);
-open ( my $file, "<", "$full_path" ) or die " Can't open the text file. ($!)\n";
+open ( my $file_read, "<", "$full_path" ) or die " Can't open the text file for reading. ($!)\n";
+
+my $path = qx/dirname $full_path/;
+chomp($path);
+system("touch $path/output.txt");
+open ( my $file_write, "<", "$path/output.txt" ) or die " Can't open the text file for writing. ($!)\n";
 
 print "Enter the word which you want to change\n";
 my $old_word = <STDIN>;
@@ -27,3 +32,9 @@ chomp($old_word);
 print "Enter a new word to replace the old one\n";
 my $new_word = <STDIN>;
 chomp($new_word);
+
+while (my $line = <$file_read>){
+$line =~ s/$old_word/$new_word/g;
+print $file_write $line;
+}
+print "\n New text file "output.txt" created in directory $path \n";

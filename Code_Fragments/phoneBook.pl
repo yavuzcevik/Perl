a1e800283a00d236a82cd5131abd0373050f6b65
yavuzcevik <yvzcevik@gmail.com>
2018-07-08 23:29:36 +0300

    Created Phone Book

--- Code_Fragments/phoneBook.pl
+++ Code_Fragments/phoneBook.pl
@@ -2,3 +2,26 @@
 
 use strict;
 use warnings;
+
+
+print<<"EOF";
+		Welcome to Phone Book
+
+Press the number:
+	
+	1 - Add a contact 
+	2 - Search a contact
+
+EOF
+
+my $choice = <STDIN>;
+
+if ($choice == 1){ 
+}
+
+elsif($choice == 2){
+}
+
+else {
+}
+

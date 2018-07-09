34e60927aae8a230c68db121cec4d0cceba19a9e
Halil Yavuz Çevik <yvzcevik@gmail.com>
2018-07-09 15:49:35 +0300

    Update phoneBook.pl

--- Code_Fragments/phoneBook.pl
+++ Code_Fragments/phoneBook.pl
@@ -20,7 +20,8 @@ system("touch $path/phoneBook.txt");
 system("chmod 666 $path/phoneBook.txt");
 my @alphabet = ('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z');
 my $count = 0;
-open ( $file_write, ">", "$path/phoneBook.txt" ) or die "Can't open the text file for writing ($!)\n";
+open ( my $file_write, ">", "$path/phoneBook.txt" ) or die "Can't open the text file for writing ($!)\n";
+open ( my $file_read, "<", "$path/phoneBook.txt" ) or die "Can't open the text file for reading ($!)\n";
 
 while(<@alphabet>){
 print $file_write "---------- $alphabet[$count] ----------", "\n";
@@ -30,11 +31,34 @@ $count+=1;
 my $choice = <STDIN>;
 
 if ($choice == 1){ 
+	print "Enter the contact name\n";
 }
 
 elsif($choice == 2){
+	print<<"EOF";
+Press the number:
+	
+	1 - Search by name 
+	2 - Search by number
+
+EOF
+	my $search_choice = <STDIN>;
+	chomp($search_choice);
+
+	if($search_choice == 1){
+	}
+
+	elsif($search_choice ==2){
+	}
+
+	else {
+	print " Please press 1 or 2 \n";
+	}
+
+
 }
 
 else {
+print " Please press 1 or 2 \n";
 }
 

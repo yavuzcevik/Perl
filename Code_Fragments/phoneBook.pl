461cf4fe4e7d855eed964b317833c48639deb5b9
Halil Yavuz Çevik <yvzcevik@gmail.com>
2018-07-11 16:32:36 +0300

    Update phoneBook.pl

--- Code_Fragments/phoneBook.pl
+++ Code_Fragments/phoneBook.pl
@@ -18,20 +18,29 @@ my $path = qx/dirname phoneBook.pl/;
 chomp($path);
 system("touch $path/phoneBook.txt");
 system("chmod 666 $path/phoneBook.txt");
-my @alphabet = ('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z');
+my @alphabet = ('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
 my $count = 0;
 open ( my $file_write, ">", "$path/phoneBook.txt" ) or die "Can't open the text file for writing ($!)\n";
 open ( my $file_read, "<", "$path/phoneBook.txt" ) or die "Can't open the text file for reading ($!)\n";
 
 while(<@alphabet>){
-print $file_write "---------- $alphabet[$count] ----------", "\n";
+print $file_write $alphabet[$count], "\n";
 $count+=1;
 };
-
+$count = 0;
 my $choice = <STDIN>;
 
 if ($choice == 1){ 
 	print "Enter the contact name\n";
+	my $contact_name = <STDIN>;
+	chomp($contact_name);
+	my @contact_name_array = split //, $contact_name;
+	print "Enter the contact number\n";
+	my $contact_number = <STDIN>;
+	chomp($contact_number);
+	
+	#while(<>){
+	#	if($contact_name_array[0] eq $_){};
 }
 
 elsif($choice == 2){
@@ -52,7 +61,7 @@ EOF
 	}
 
 	else {
-	print " Please press 1 or 2 \n";
+	print "Please press 1 or 2 \n";
 	}
 
 

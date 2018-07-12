33f6cece1a3f4674727c04aceba7f53e57cbd7ec
Halil Yavuz Çevik <yvzcevik@gmail.com>
2018-07-12 11:48:46 +0300

    Update phoneBook.pl

--- Code_Fragments/phoneBook.pl
+++ Code_Fragments/phoneBook.pl
@@ -16,32 +16,39 @@ EOF
 
 my $path = qx/dirname phoneBook.pl/;
 chomp($path);
-system("touch $path/phoneBook.txt");
-system("chmod 666 $path/phoneBook.txt");
-my @alphabet = ('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
-my $count = 0;
-open ( my $file_write, ">", "$path/phoneBook.txt" ) or die "Can't open the text file for writing ($!)\n";
-open ( my $file_read, "<", "$path/phoneBook.txt" ) or die "Can't open the text file for reading ($!)\n";
 
-while(<@alphabet>){
-print $file_write $alphabet[$count], "\n";
-$count+=1;
+if(!(-e "$path/phoneBook.txt)){
+	my @alphabet = ('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z');
+	my $count = 0;
+	open ( my $file_write, ">", "$path/phoneBook.txt" ) or die "Can't open the text file for writing ($!)\n";
+	while(<@alphabet>){
+		print $file_write $alphabet[$count], "\n";
+		$count+=1;
+	};
+	close($file_write);
+	$count = 0;
 };
-$count = 0;
+
+open ( my $file_read, "<", "$path/phoneBook.txt" ) or die "Can't open the text file for reading ($!)\n";
 my $choice = <STDIN>;
 
 if ($choice == 1){ 
 	print "Enter the contact name\n";
 	my $contact_name = <STDIN>;
 	chomp($contact_name);
+	$contact_name = uc ($contact_name);
 	my @contact_name_array = split //, $contact_name;
 	print "Enter the contact number\n";
 	my $contact_number = <STDIN>;
 	chomp($contact_number);
 	
-	while(<@alphabet>){
-		if($contact_name_array[0] eq $_){
-			print $file_write $contact_name, "\n";
+	while(<$file_read>){
+		my $letter = $_;
+		chomp($letter);
+		if($contact_name_array[0] eq $letter){
+			open ( my $file_write, ">>", "$path/phoneBook.txt" ) or die "Can't open the text file for writing ($!)\n";
+			print $file_write "$contact_name	$contact_number", "\n";
+			close($file_write);
 		};	
 	}
 }
